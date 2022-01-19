ca10db140e06aea6da13889dfab42ecbb60dee36
Komodeo <onlythebest@gmail.com>
2022-01-18 17:56:39 -0800

    game character
    2. Add your game character to the sketch [0 marks]
    •	As previously, use the`gameChar_x` and `gameChar_y` variables to control the position of the character on the canvas.
    •	You only need to use your facing-forward character for this task.
    •	Check your character moves left and right when the corresponding keys are pressed.

--- game4/sketch.js
+++ game4/sketch.js
@@ -17,11 +17,10 @@ var clouds;
 var mountains;
 var trees_x;
 
-function setup()
-{
+function setup() {
 	createCanvas(1024, 576);
-	floorPos_y = height * 3/4;
-	gameChar_x = width/2;
+	floorPos_y = height * 3 / 4;
+	gameChar_x = width / 2;
 	gameChar_y = floorPos_y;
 
 	// Boolean variables to control the movement of the game character.
@@ -34,13 +33,12 @@ function setup()
 	// Initialise arrays of scenery objects.
 }
 
-function draw()
-{
+function draw() {
 	background(100, 155, 255); // fill the sky blue
 
 	noStroke();
 	fill(0, 155, 0);
-	rect(0, floorPos_y, width, height/4); // draw some green ground
+	rect(0, floorPos_y, width, height / 4); // draw some green ground
 
 	// Draw clouds.
 
@@ -54,59 +52,77 @@ function draw()
 
 	// Draw the game character - this must be last
 
+	//begin character drawing
+
+	//body
+	stroke(0);
+	fill(153, 51, 47);
+	triangle(gameChar_x - 13, gameChar_y - 7, gameChar_x + 13, gameChar_y - 7 - 10, gameChar_x, gameChar_y - 64);
+
+	//feet
+	fill(71, 43, 12);
+	ellipse(gameChar_x - 13, gameChar_y - 7, 20); //left
+	ellipse(gameChar_x + 13, gameChar_y - 7 - 10, 20); //right
+
+	//head
+	fill(235, 152, 181);
+	ellipse(gameChar_x, gameChar_y - 52, 30);
+
+	//mouth
+	fill(0);
+	ellipse(gameChar_x, gameChar_y - 44, 5 - 2);
+
+	//left eye
+	line(gameChar_x - 10, gameChar_y - 52, gameChar_x - 7, gameChar_y - 55);
+	line(gameChar_x - 4 - 6, gameChar_y - 52 - 6, gameChar_x - 7, gameChar_y - 55);
+
+	//right eye
+	line(gameChar_x + 10 - 6, gameChar_y - 52 - 6, gameChar_x + 7, gameChar_y - 55);
+	line(gameChar_x + 4, gameChar_y - 52, gameChar_x + 7, gameChar_y - 55);
+
+	//end character drawing
+
 	//////// Game character logic ///////
 	// Logic to move
 
-	if(isLeft)
-	{
-		if(gameChar_x > width * 0.2)
-		{
+	if (isLeft) {
+		if (gameChar_x > width * 0.2) {
 			gameChar_x -= 5;
 		}
-		else
-		{
+		else {
 			scrollPos += 5;
 		}
 	}
 
-	if(isRight)
-	{
-		if(gameChar_x < width * 0.8)
-		{
-			gameChar_x  += 5;
+	if (isRight) {
+		if (gameChar_x < width * 0.8) {
+			gameChar_x += 5;
 		}
-		else
-		{
+		else {
 			scrollPos -= 5; // negative for moving against the background
 		}
 
 	}
 }
 
-function keyPressed()
-{
+function keyPressed() {
 
-	if(key == 'A' || keyCode == 37)
-	{
+	if (key == 'A' || keyCode == 37) {
 		isLeft = true;
 	}
 
-	if(key == 'D' || keyCode == 39)
-	{
+	if (key == 'D' || keyCode == 39) {
 		isRight = true;
 	}
 
 }
 
-function keyReleased()
-{
-	if(key == 'A' || keyCode == 37)
-	{
+function keyReleased() {
+	if (key == 'A' || keyCode == 37) {
 		isLeft = false;
 	}
 
-	if(key == 'D' || keyCode == 39)
-	{
+	if (key == 'D' || keyCode == 39) {
 		isRight = false;
 	}
 }

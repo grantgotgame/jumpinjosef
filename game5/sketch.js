25cd0662f9ecc84fd68e7d6fa491502805c3c120
Komodeo <onlythebest@gmail.com>
2022-01-21 19:59:55 -0800

    add game character controls
    2. Add game character controls [1 mark]
    
    Copy and paste the code which draws your game character from part 3_interaction into the function `drawGameChar`.
    
    Check that your game character appears in your sketch.
    
    Copy and paste the code inside keyPressed and keyReleased functions from part 3_interaction into the same functions here.
    
    Test that your game character can move left and right and jump. NB. they won't fall back to the ground just yet.
    
    Check that the animations look correct.

--- game5/sketch.js
+++ game5/sketch.js
@@ -15,11 +15,10 @@ var isRight;
 var isFalling;
 var isPlummeting;
 
-function setup()
-{
+function setup() {
 	createCanvas(1024, 576);
-	floorPos_y = height * 3/4;
-	gameChar_x = width/2;
+	floorPos_y = height * 3 / 4;
+	gameChar_x = width / 2;
 	gameChar_y = floorPos_y;
 
 	// Variable to control the background scrolling.
@@ -38,13 +37,12 @@ function setup()
 	// Initialise arrays of scenery objects.
 }
 
-function draw()
-{
+function draw() {
 	background(100, 155, 255); // fill the sky blue
 
 	noStroke();
-	fill(0,155,0);
-	rect(0, floorPos_y, width, height/4); // draw some green ground
+	fill(0, 155, 0);
+	rect(0, floorPos_y, width, height / 4); // draw some green ground
 
 	// Draw clouds.
 
@@ -57,30 +55,24 @@ function draw()
 	// Draw collectable items.
 
 	// Draw game character.
-	
+
 	drawGameChar();
 
 	// Logic to make the game character move or the background scroll.
-	if(isLeft)
-	{
-		if(gameChar_x > width * 0.2)
-		{
+	if (isLeft) {
+		if (gameChar_x > width * 0.2) {
 			gameChar_x -= 5;
 		}
-		else
-		{
+		else {
 			scrollPos += 5;
 		}
 	}
 
-	if(isRight)
-	{
-		if(gameChar_x < width * 0.8)
-		{
-			gameChar_x  += 5;
+	if (isRight) {
+		if (gameChar_x < width * 0.8) {
+			gameChar_x += 5;
 		}
-		else
-		{
+		else {
 			scrollPos -= 5; // negative for moving against the background
 		}
 	}
@@ -96,18 +88,48 @@ function draw()
 // Key control functions
 // ---------------------
 
-function keyPressed(){
+function keyPressed() {
 
-	console.log("press" + keyCode);
-	console.log("press" + key);
+	// if statements to control the animation of the character when
+	// keys are pressed.
+
+	if (keyCode == LEFT_ARROW) {
+		isLeft = true;
+	}
+
+	if (keyCode == RIGHT_ARROW) {
+		isRight = true;
+	}
+
+	if (keyCode == 32 && gameChar_y == floorPos_y) {
+		gameChar_y -= 100;
+	}
+
+	//open up the console to see how these work
+	console.log("keyPressed: " + key);
+	console.log("keyPressed: " + keyCode);
+	console.log("isLeft: " + isLeft);
+	console.log("isRight: " + isRight);
 
 }
 
-function keyReleased()
-{
+function keyReleased() {
 
-	console.log("release" + keyCode);
-	console.log("release" + key);
+	// if statements to control the animation of the character when
+	// keys are released.
+
+	if (keyCode == LEFT_ARROW) {
+		isLeft = false;
+	}
+
+	if (keyCode == RIGHT_ARROW) {
+		isRight = false;
+	}
+
+	console.log("keyReleased: " + key);
+	console.log("keyReleased: " + keyCode);
+	console.log("isLeft: " + isLeft);
+	console.log("isRight: " + isRight);
 
 }
 
@@ -118,9 +140,217 @@ function keyReleased()
 
 // Function to draw the game character.
 
-function drawGameChar()
-{
-	// draw game character
+function drawGameChar() {
+
+	// jumping-left code
+	if (isLeft && isFalling) {
+
+		//begin character drawing
+
+		//body
+		stroke(0);
+		fill(153, 51, 47);
+		triangle(gameChar_x - 13, gameChar_y - 7 - 10, gameChar_x + 13, gameChar_y - 7, gameChar_x - 8, gameChar_y - 64);
+
+		//feet
+		fill(71, 43, 12);
+		ellipse(gameChar_x - 13, gameChar_y - 7 - 10, 20); //left
+		ellipse(gameChar_x + 13, gameChar_y - 7, 20); //right
+
+		//head
+		fill(235, 152, 181);
+		ellipse(gameChar_x - 8, gameChar_y - 52, 30);
+
+		//mouth
+		fill(0);
+		ellipse(gameChar_x - 8, gameChar_y - 44, 5 - 4);
+
+		//left eye
+		line(gameChar_x - 8 - 10 + 6, gameChar_y - 52 - 6, gameChar_x - 8 - 7, gameChar_y - 55);
+		line(gameChar_x - 8 - 4, gameChar_y - 52, gameChar_x - 8 - 7, gameChar_y - 55);
+
+		//right eye
+		line(gameChar_x - 8 + 10, gameChar_y - 52, gameChar_x - 8 + 7, gameChar_y - 55);
+		line(gameChar_x - 8 + 4 + 6, gameChar_y - 52 - 6, gameChar_x - 8 + 7, gameChar_y - 55);
+
+		//end character drawing
+
+	}
+
+	// jumping-right code
+	else if (isRight && isFalling) {
+
+		//begin character drawing
+
+		//body
+		stroke(0);
+		fill(153, 51, 47);
+		triangle(gameChar_x - 13, gameChar_y - 7, gameChar_x + 13, gameChar_y - 7 - 10, gameChar_x + 8, gameChar_y - 64);
+
+		//feet
+		fill(71, 43, 12);
+		ellipse(gameChar_x - 13, gameChar_y - 7, 20); //left
+		ellipse(gameChar_x + 13, gameChar_y - 7 - 10, 20); //right
+
+		//head
+		fill(235, 152, 181);
+		ellipse(gameChar_x + 8, gameChar_y - 52, 30);
+
+		//mouth
+		fill(0);
+		ellipse(gameChar_x + 8, gameChar_y - 44, 5 - 4);
+
+		//left eye
+		line(gameChar_x + 8 - 10, gameChar_y - 52, gameChar_x + 8 - 7, gameChar_y - 55);
+		line(gameChar_x + 8 - 4 - 6, gameChar_y - 52 - 6, gameChar_x + 8 - 7, gameChar_y - 55);
+
+		//right eye
+		line(gameChar_x + 8 + 10 - 6, gameChar_y - 52 - 6, gameChar_x + 8 + 7, gameChar_y - 55);
+		line(gameChar_x + 8 + 4, gameChar_y - 52, gameChar_x + 8 + 7, gameChar_y - 55);
+
+		//end character drawing
+
+	}
+
+	// walking left code
+	else if (isLeft) {
+
+		//begin character drawing
+
+		//body
+		stroke(0);
+		fill(153, 51, 47);
+		triangle(gameChar_x - 13, gameChar_y - 7 - 10, gameChar_x + 13, gameChar_y - 7, gameChar_x, gameChar_y - 64);
+
+		//feet
+		fill(71, 43, 12);
+		ellipse(gameChar_x - 13, gameChar_y - 7 - 10, 20); //left
+		ellipse(gameChar_x + 13, gameChar_y - 7, 20); //right
+
+		//head
+		fill(235, 152, 181);
+		ellipse(gameChar_x, gameChar_y - 52, 30);
+
+		//mouth
+		fill(0);
+		ellipse(gameChar_x, gameChar_y - 44, 5 - 2);
+
+		//left eye
+		line(gameChar_x - 10 + 6, gameChar_y - 52 - 6, gameChar_x - 7, gameChar_y - 55);
+		line(gameChar_x - 4, gameChar_y - 52, gameChar_x - 7, gameChar_y - 55);
+
+		//right eye
+		line(gameChar_x + 10, gameChar_y - 52, gameChar_x + 7, gameChar_y - 55);
+		line(gameChar_x + 4 + 6, gameChar_y - 52 - 6, gameChar_x + 7, gameChar_y - 55);
+
+		//end character drawing
+
+	}
+
+	// walking right code
+	else if (isRight) {
+
+		//begin character drawing
+
+		//body
+		stroke(0);
+		fill(153, 51, 47);
+		triangle(gameChar_x - 13, gameChar_y - 7, gameChar_x + 13, gameChar_y - 7 - 10, gameChar_x, gameChar_y - 64);
+
+		//feet
+		fill(71, 43, 12);
+		ellipse(gameChar_x - 13, gameChar_y - 7, 20); //left
+		ellipse(gameChar_x + 13, gameChar_y - 7 - 10, 20); //right
+
+		//head
+		fill(235, 152, 181);
+		ellipse(gameChar_x, gameChar_y - 52, 30);
+
+		//mouth
+		fill(0);
+		ellipse(gameChar_x, gameChar_y - 44, 5 - 2);
+
+		//left eye
+		line(gameChar_x - 10, gameChar_y - 52, gameChar_x - 7, gameChar_y - 55);
+		line(gameChar_x - 4 - 6, gameChar_y - 52 - 6, gameChar_x - 7, gameChar_y - 55);
+
+		//right eye
+		line(gameChar_x + 10 - 6, gameChar_y - 52 - 6, gameChar_x + 7, gameChar_y - 55);
+		line(gameChar_x + 4, gameChar_y - 52, gameChar_x + 7, gameChar_y - 55);
+
+		//end character drawing
+
+	}
+
+	// jumping facing forwards code
+	else if (isFalling || isPlummeting) {
+
+		//begin character drawing
+
+		//body
+		stroke(0);
+		fill(153, 51, 47);
+		triangle(gameChar_x - 13, gameChar_y - 7 - 5, gameChar_x + 13, gameChar_y - 7, gameChar_x, gameChar_y - 64);
+
+		//feet	
+		fill(71, 43, 12);
+		ellipse(gameChar_x - 13, gameChar_y - 7 - 5, 20); //left
+		ellipse(gameChar_x + 13, gameChar_y - 7, 20); //right
+
+		//head
+		fill(235, 152, 181);
+		ellipse(gameChar_x, gameChar_y - 52, 30);
+
+		//mouth
+		fill(0);
+		ellipse(gameChar_x, gameChar_y - 44, 5 - 4);
+
+		//left eye
+		line(gameChar_x - 10, gameChar_y - 6 - 52, gameChar_x - 7, gameChar_y - 55);
+		line(gameChar_x - 4, gameChar_y - 6 - 52, gameChar_x - 7, gameChar_y - 55);
+
+		//right eye
+		line(gameChar_x + 10, gameChar_y - 6 - 52, gameChar_x + 7, gameChar_y - 55);
+		line(gameChar_x + 4, gameChar_y - 6 - 52, gameChar_x + 7, gameChar_y - 55);
+
+		//end character drawing
+
+	}
+
+	// standing front facing code
+	else {
+
+		//begin character drawing
+
+		//body
+		stroke(0);
+		fill(153, 51, 47);
+		triangle(gameChar_x - 13, gameChar_y - 7, gameChar_x + 13, gameChar_y - 7, gameChar_x, gameChar_y - 64);
+
+		//feet
+		fill(71, 43, 12);
+		ellipse(gameChar_x - 13, gameChar_y - 7, 20); //left
+		ellipse(gameChar_x + 13, gameChar_y - 7, 20); //right
+
+		//head
+		fill(235, 152, 181);
+		ellipse(gameChar_x, gameChar_y - 52, 30);
+
+		//mouth
+		fill(0);
+		ellipse(gameChar_x, gameChar_y - 44, 5);
+
+		//left eye
+		line(gameChar_x - 10, gameChar_y - 52, gameChar_x - 7, gameChar_y - 55);
+		line(gameChar_x - 4, gameChar_y - 52, gameChar_x - 7, gameChar_y - 55);
+
+		//right eye
+		line(gameChar_x + 10, gameChar_y - 52, gameChar_x + 7, gameChar_y - 55);
+		line(gameChar_x + 4, gameChar_y - 52, gameChar_x + 7, gameChar_y - 55);
+
+		//end character drawing
+
+	}
 }
 
 // ---------------------------
@@ -140,15 +370,13 @@ function drawGameChar()
 
 // Function to draw canyon objects.
 
-function drawCanyon(t_canyon)
-{
+function drawCanyon(t_canyon) {
 
 }
 
 // Function to check character is over a canyon.
 
-function checkCanyon(t_canyon)
-{
+function checkCanyon(t_canyon) {
 
 }
 
@@ -158,14 +386,12 @@ function checkCanyon(t_canyon)
 
 // Function to draw collectable objects.
 
-function drawCollectable(t_collectable)
-{
+function drawCollectable(t_collectable) {
 
 }
 
 // Function to check character has collected an item.
 
-function checkCollectable(t_collectable)
-{
+function checkCollectable(t_collectable) {
 
 }

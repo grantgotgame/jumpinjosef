cd9fbdcae20ec2a8d90c82fcd38866a1f3f671b4
Komodeo <onlythebest@gmail.com>
2022-01-21 20:26:28 -0800

    mountains, clouds, and trees
    4. Render background items [2 mark]
    
    From part 4_scrollingBackground, copy and paste the arrays `trees_x`, `clouds`, and `mountains`.
    
    Copy and paste the `for loops` for each of these items into the `draw` function.
    
    Now we are going to refactor this code.
    
    Create three new functions: `drawClouds`, `drawMountains`, `drawTrees`.
    
    Cut and paste each `for loop` into the relevant function.
    
    In the `draw` function, call each of the three new functions (e.g. `drawClouds`).
    
    You should now see all of the above items drawn to the screen.

--- game5/sketch.js
+++ game5/sketch.js
@@ -9,6 +9,7 @@ var gameChar_y;
 var floorPos_y;
 var scrollPos;
 var gameChar_world_x;
+var treePos_y;
 
 var isLeft;
 var isRight;
@@ -20,6 +21,7 @@ function setup() {
 	floorPos_y = height * 3 / 4;
 	gameChar_x = width / 2;
 	gameChar_y = floorPos_y;
+	treePos_y = floorPos_y - 250;
 
 	// Variable to control the background scrolling.
 	scrollPos = 0;
@@ -35,6 +37,65 @@ function setup() {
 	isPlummeting = false;
 
 	// Initialise arrays of scenery objects.
+
+	//initialize trees
+	trees_x = [200, 350, 700, 850, 1000, 1150, -150, -300, 1300, 1450, 1600, 1750, 2500, 2650, 3000, 3150];
+
+	//initialize clouds
+	clouds = [{
+		x_pos: 200,
+		y_pos: 50,
+		size: 50
+	}, {
+		x_pos: 500,
+		y_pos: 150,
+		size: 100
+	}, {
+		x_pos: 700,
+		y_pos: 50,
+		size: 75
+	}, {
+		x_pos: 900,
+		y_pos: 200,
+		size: 25
+	}, {
+		x_pos: -500,
+		y_pos: 175,
+		size: 120
+	}, {
+		x_pos: 1200,
+		y_pos: 80,
+		size: 90
+	}, {
+		x_pos: 1500,
+		y_pos: 120,
+		size: 134
+	}, {
+		x_pos: 1900,
+		y_pos: 250,
+		size: 40
+	}];
+
+	//initialize mountains
+	mountains = [{
+		x_pos: 75,
+		size: 100
+	}, {
+		x_pos: 150,
+		size: 200
+	}, {
+		x_pos: 750,
+		size: 50
+	}, {
+		x_pos: 1900,
+		size: 900
+	}, {
+		x_pos: -750,
+		size: 250
+	}, {
+		x_pos: 7520,
+		size: 5000
+	}];
 }
 
 function draw() {
@@ -45,10 +106,19 @@ function draw() {
 	rect(0, floorPos_y, width, height / 4); // draw some green ground
 
 	// Draw clouds.
+	for (i = 0; i < clouds.length; i++) {
+		drawClouds();
+	}
 
 	// Draw mountains.
+	for (i = 0; i < mountains.length; i++) {
+		drawMountains();
+	}
 
 	// Draw trees.
+	for (i = 0; i < trees_x.length; i++) {
+		drawTrees();
+	}
 
 	// Draw canyons.
 
@@ -366,10 +436,55 @@ function drawGameChar() {
 
 // Function to draw cloud objects.
 
+function drawClouds() {
+	//begin cloud drawing
+
+	fill(255);
+	ellipse(clouds[i].x_pos + clouds[i].size * 0.5, clouds[i].y_pos, clouds[i].size * 1.2);
+	ellipse(clouds[i].x_pos + clouds[i].size, clouds[i].y_pos, clouds[i].size);
+	ellipse(clouds[i].x_pos, clouds[i].y_pos, clouds[i].size);
+
+	//end cloud drawing
+}
+
 // Function to draw mountains objects.
+
+function drawMountains() {
+	//begin mountain drawing
+
+	fill(205, 186, 217);
+	triangle(mountains[i].x_pos + mountains[i].size, floorPos_y - mountains[i].size * 1.76, mountains[i].x_pos + mountains[i].size * 2, floorPos_y, mountains[i].x_pos, floorPos_y);
+	fill(255);
+	triangle(mountains[i].x_pos + mountains[i].size, floorPos_y - mountains[i].size * 1.76, mountains[i].x_pos + mountains[i].size * 1.5, floorPos_y - mountains[i].size * 0.87, mountains[i].x_pos + mountains[i].size * 0.5, floorPos_y - mountains[i].size * 0.87);
+
+	fill(205, 186, 217);
+	triangle(mountains[i].x_pos + mountains[i].size * 1.75, floorPos_y - mountains[i].size * 1.32, mountains[i].x_pos + mountains[i].size * 3, floorPos_y, mountains[i].x_pos + mountains[i].size * 0.75, floorPos_y);
+	fill(255);
+	triangle(mountains[i].x_pos + mountains[i].size * 1.75, floorPos_y - mountains[i].size * 1.32, mountains[i].x_pos + mountains[i].size * 2.17, floorPos_y - mountains[i].size * 0.87, mountains[i].x_pos + mountains[i].size * 1.4, floorPos_y - mountains[i].size * 0.87);
+
+	//end mountain drawing
+}
 
 // Function to draw trees objects.
 
+function drawTrees() {
+	//begin tree drawing
+
+	//trunk
+	fill(43, 23, 15);
+	rect(trees_x[i], treePos_y, 50, 250);
+
+	//head
+	fill(10, 43, 19);
+	ellipse(trees_x[i] + 25, treePos_y, 200);
+
+	//fruit
+	fill(186, 17, 11);
+	ellipse(trees_x[i], treePos_y, 20);
+	ellipse(trees_x[i] + 60, treePos_y + 50, 20);
+
+	//end tree drawing
+}
 
 // ---------------------------------
 // Canyon render and check functions
